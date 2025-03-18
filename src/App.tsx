ede46c7c0d640cbb30e085bae01718151769ca7f
itsbasilsaman <itsbasilsaman@gmail.com>
2025-03-18 11:32:57 +0530

    Route changes made

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ export const App: React.FC = React.memo(() => {
 
   // Redirect to login page if token is missing and not already on the login page
   if (!accessToken && !isLoginRoute) {
-    return <Navigate to="/admin/login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return (
@@ -48,7 +48,7 @@ export const App: React.FC = React.memo(() => {
       <Toaster position="top-center" />
       <Suspense fallback={<Loading />}>
         <Routes>
-          <Route path="/admin/login"  element={<AdminLogin />} />
+          <Route path="/"  element={<AdminLogin />} />
         </Routes>
         {!isLoginRoute && isLoggedGGAdmin && (
           <DefaultLayout>
